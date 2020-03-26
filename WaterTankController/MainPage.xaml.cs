0c638ead21c4d5d2efadd8edf2c4656f52e0d635
kishore Chowdary <lovelykishore02@outlook.com>
2020-03-26 12:47:43 +0530

    Initial_Commit

---
+++ WaterTankController/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
+using Windows.System;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WaterTankController
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        GpioPin alarm;
+        public MainPage()
+        {            
+            this.InitializeComponent();
+            Loaded += MainPage_Load;
+            ShutdownManager.BeginShutdown(Windows.System.ShutdownKind.Shutdown, TimeSpan.FromSeconds(1));
+        }
+        private void MainPage_Load(object sender, RoutedEventArgs e)
+        {
+            var controller = GpioController.GetDefault();            
+            alarm = controller.OpenPin(26);           
+            alarm.SetDriveMode(GpioPinDriveMode.Output);
+            alarm.Write(GpioPinValue.Low);
+            Task.Delay(787000).Wait();
+            alarm.Write(GpioPinValue.High);            
+        }
+    }
+}
